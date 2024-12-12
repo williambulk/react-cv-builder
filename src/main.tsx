5f4d6d048fd2272aa14acf9acef68acbea09005f
William Kunitake <william.kunitake@gmail.com>
2024-12-12 10:36:37 -0800

    Imported and included General and Professional components

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import General from './components/General.tsx';
+import Professional from './components/Professional.tsx';
 import Educational from './components/Educational.tsx';
-// import General from './components/General.tsx';
-// import Professional from './components/Professional.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <General />
+    <Professional />
     <Educational />
   </StrictMode>,
 )
