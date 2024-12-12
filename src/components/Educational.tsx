fedc23ab152d3b06ebaf865f647907b0eca761b9
William Kunitake <william.kunitake@gmail.com>
2024-12-11 20:27:41 -0800

    Changed state name from "date" to "graduation"

--- src/components/Educational.tsx
+++ src/components/Educational.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react'
 
-function Educational( { schoolName = "University Federal of Parana", schoolStudy = "Bachelor of Science", schoolDate = "April 2012" } ) {
+function Educational( { schoolName = "University Federal of Parana", schoolStudy = "Bachelor of Science", schoolGraduation = "April 2012" } ) {
 
     // Creating the individual useState
     const [name, setName] = useState(schoolName);
     const [study, setStudy] = useState(schoolStudy);
-    const [date, setDate] = useState(schoolDate);
+    const [graduation, setGraduation] = useState(schoolGraduation);
 
     // Defining Edit Mode
     const [editMode, setEditMode] = useState(false);
@@ -20,7 +20,7 @@ function Educational( { schoolName = "University Federal of Parana", schoolStudy
             <div className="cv-builder-input">
                 <label>School Name: { ! editMode ? name : <input type="text" value={ name } placeholder="Insert the School Name" onChange={ (event) => setName(event.target.value) } /> }</label>
                 <label>Study: { ! editMode ? study : <input type="text" value={ study } placeholder="Insert the Study" onChange={ (event) => setStudy(event.target.value) } /> }</label>
-                <label>Date: { ! editMode ? date : <input type="text" value={ date } placeholder="Insert the Period of Study" onChange={ (event) => setDate(event.target.value) } /> }</label>
+                <label>Date: { ! editMode ? graduation : <input type="text" value={ graduation } placeholder="Insert the Period of Study" onChange={ (event) => setGraduation(event.target.value) } /> }</label>
             </div>
             <button onClick={ handleEditInfo }>{ editMode ? "Save Info" : "Edit Info" }</button>
         </>
